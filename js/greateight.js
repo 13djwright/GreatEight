d5e5bcf60f5584cb066a912b6b4cab60166e957b
Devin Wright <13djwright@gmail.com>
2015-02-27 23:09:43 -0500

    implemented creating of a deck

--- js/greateight.js
+++ js/greateight.js
@@ -1,17 +1,56 @@
-function Card(value, image) {
+function Card(value) {
 	this.value = value;
-	this.image = image;
+	this.image = "./images/"+value+".png";
 	this.visible;
 }
 
 function Deck() {
 	this.cards = new Array();
-	this.cardsPlayer = new Array();
+	this.cardsPlayed = new Array();
 	
 	this.makeDeck = makeDeck;
 	this.shuffle = shuffleDeck;
 	this.deal = dealCard;
 	this.cardsLeft = getCardsLeft;
 	
+}
 
+/*
+makeDeck(): Initializes a standard deck of playing cards
+*/
+makeDeck() {
+	this.cards = new Array(16);
+	for(var i = 0; i < 16; i++) {
+		if (i<5) {
+			this.cards[i] = new Card(1);
+		}
+		
+		if (i >= 5 && i < 7) {
+			this.cards[i] = new Card(2);
+		}
+		
+		if (i >= 7 && i < 9) {
+			this.cards[i] = new Card(3);
+		}
+		
+		if (i >= 9 && i < 11) {
+			this.cards[i] = new Card(4);
+		}
+		
+		if (i >= 11 && i < 13) {
+			this.cards[i] = new Card(5);
+		}
+		
+		if (i == 13) {
+			this.cards[i] = new Card(6);
+		}
+		
+		if (i == 14) { 
+			this.cards[i] = new Card(7);
+		}
+		
+		if (i == 15) {
+			this.cards[i] = new Card(8);
+		}
+	}
 }
