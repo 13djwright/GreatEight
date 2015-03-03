eb991ad0d9704f0dcc25d5861b02667caf492841
Devin Wright <13djwright@gmail.com>
2015-03-02 23:47:38 -0500

    Turned off loading function for faster testing. Updated to create a new Game.

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,17 @@
 var time = 1;
-var deck;
-
+var game;
 function loadGame() {
 	//120 because the loading bar would disappear before done "loading"
-	if (time>=110) {
+	//if (time>=110) {
 		clearInterval(interval);
 		//remove the bar and display the game
 		document.getElementById("progressbar").style.display="none";
-		deck = new Deck();
-		deck.makeDeck();
-		deck.shuffle(2);
-	}
+		game = new Game();
+	//}
 	document.getElementById("bar").style.width=time+"%";
 	time++;
 }
-
 var interval=setInterval(function () {loadGame()}, 100);
+
+
 
