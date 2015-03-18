9d3ea2a519886f5453685ac8c99acf928e410665
Devin Wright <13djwright@gmail.com>
2015-03-18 19:46:03 -0400

    Removed loading bar

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,2 @@
 var time = 1;
-var game;
-function loadGame() {
-	//120 because the loading bar would disappear before done "loading"
-	//if (time>=110) {
-		clearInterval(interval);
-		//remove the bar and display the game
-		document.getElementById("progressbar").style.display="none";
-		game = new Game();
-	//}
-	document.getElementById("bar").style.width=time+"%";
-	time++;
-}
-var interval=setInterval(function () {loadGame()}, 100);
-
-
-
+var game = new Game();
