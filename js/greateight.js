e907a9b8da8a7184334b9a127c96a5b09c6cb8cc
austintaing <arta226@g.uky.edu>
2015-03-31 06:38:33 +0000

    targeting decisions for all cases first round complete

--- js/greateight.js
+++ js/greateight.js
@@ -593,6 +593,8 @@ function chooseTarget(c, playerNum) {
 		break;
 	
 	case 5:
+	// cases 5 and 6 have the same targeting algorithm
+	case 6:
 		if(validTargets.length>1) {
 			target = validTargets[0];
 			for(i=1; i<validTargets.length; i++)
@@ -602,7 +604,6 @@ function chooseTarget(c, playerNum) {
 		}
 		break;
 	
-	case 6:
 		
 	}	
 	
