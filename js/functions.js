f78cebe74161acad8de0ca67b41d7c695ca27fc6
unknown <dwright@dwright-PC.triu.com>
2015-04-03 15:59:41 -0400

    added a Game Log modal along with a function to append text in that modal (addToGameLog(text) )

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,9 @@
 var time = 1;
 var game = new Game();
+
+function addToGameLog(text) {
+	var node = document.createElement("p");
+	var textnode = document.createTextNode(text);
+	node.appendChild(textnode);
+	document.getElementById("gameLogBody").appendChild(node);
+}
