d0395b1cc8fc898d7bd0784b08cbdf74d717cc84
Devin Wright <13djwright@gmail.com>
2015-04-19 23:55:46 -0400

    added in toast notification

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,18 @@ var time = 1;
 var game = new Game();
 
 function addToGameLog(text) {
+	$.notify({
+		message: text
+	},{
+		type: 'info',
+		placement: {
+			from: 'top',
+			align: 'center'
+		},
+		delay: 5000,
+		allow_dismiss: true
+	}
+	);
 	var node = document.createElement("p");
 	var textnode = document.createTextNode(text);
 	node.appendChild(textnode);
